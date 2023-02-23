c41ab000621d94caf77ef27ddcf03906486b6acb
Enver Yilmaz <109732583+enverbey@users.noreply.github.com>
2023-02-23 23:52:27 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: envyilma <envyilma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 19:51:02 by envyilma          #+#    #+#             */
+/*   Updated: 2023/02/23 20:28:37 by envyilma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list *args, char c);
+int	ft_int(int a);
+int	ft_hex(unsigned int a, char c);
+int	ft_point(unsigned long a, int sign);
+int	ft_string(char *str);
+int	ft_unsigned(unsigned int a);
+
+#endif
